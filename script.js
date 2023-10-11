3aedaf3f54e1ef5cd5425e1579ec97b6602015f3
Micah Wood <mxw7265@rit.edu>
2023-10-11 15:30:29 -0600

    Revert "Added couple more Temperature Converters"
    This reverts commit 2c326fbba71c1ebee118ac7b7540315d4dfa93c3.

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const temperatureInput = document.getElementById("temperature");
+    const celsiusInput = document.getElementById("celsius");
     const convertButton = document.getElementById("convert");
     const resultText = document.getElementById("result-text");
-    const fromTypeSelect = document.getElementById("conversion-from-type");
-    const toTypeSelect = document.getElementById("conversion-to-type");
 
     convertButton.addEventListener("click", function() {
-        const selectedFromOption = fromTypeSelect.value;
-        const selectedToOption = toTypeSelect.value;
-        const temperature = parseFloat(temperatureInput.value);
-
-        if (isNaN(temperature)) {
-            resultText.textContent = `Please enter a valid temperature in ${selectedFromOption}.`;
+        const celsius = parseFloat(celsiusInput.value);
+        if (!isNaN(celsius)) {
+            const fahrenheit = (celsius * 9/5) + 32;
+            resultText.textContent = `${celsius}°C is equal to ${fahrenheit.toFixed(2)}°F`;
         } else {
-            const resultTempFunction = tempFunctionMap(selectedFromOption, selectedToOption);
-            const resultTempValue = resultTempFunction(temperature);
-            resultText.textContent = `${temperature}${tempUnitMap(selectedFromOption)} is equal to ${resultTempValue.toFixed(2)}${tempUnitMap(selectedToOption)}`;
+            resultText.textContent = "Please enter a valid temperature in Celsius.";
         }
-    });
-
-    fromTypeSelect.addEventListener("change", function() {
-        const selectedFromOption = fromTypeSelect.value;
-
-        temperatureInput.setAttribute("placeholder", `Enter temperature in ${selectedFromOption}`);
     });
 });
